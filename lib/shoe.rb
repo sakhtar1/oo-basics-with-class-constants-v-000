d6d31838c24bec93a5bb886e722a2f01e4e1fc1d
Sharmina Akhtar <sakht318@gmail.com>
2017-11-16 19:45:48 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,18 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
 
-  def initialize(brand)
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand, :brands
+  BRANDS = []
+
+
+
+#Shoes::BRANDS.size simply means that inside CLass SHoes array BRANds give me
+#the size of that array or length and it will return to you an integer
+  def initialize (brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
+
   end
 
   def cobble
