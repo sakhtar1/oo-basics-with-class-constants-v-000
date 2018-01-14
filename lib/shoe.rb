a964e526a6d33b6bc4911bbdba0dd196ec2d0834
Sharmina Akhtar <sakht318@gmail.com>
2018-01-14 11:03:13 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,17 +4,12 @@ class Shoe
   attr_reader :brand, :brands
   BRANDS = []
 
-
-
 #Shoes::BRANDS.size simply means that inside CLass SHoes array BRANds give me
 #the size of that array or length and it will return to you an integer
   def initialize (brand)
     @brand = brand
-    BRANDS << brand
-    BRANDS.uniq!
-
-  end
-
+  end 
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
